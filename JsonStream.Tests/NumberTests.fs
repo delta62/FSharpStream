573ddbc911def7532efe1caf4f8dd122bf18e807
Sam Noedel <sam.noedel@gmail.com>
2018-07-04 13:08:32 -0700

    Add numeric tests

---
+++ JsonStream.Tests/NumberTests.fs
@@ -0,0 +1,40 @@
+module NumberTests
+
+open Expecto
+open FSharpx.Collections
+open Tokenizer
+open FsCheck
+open ScienceGenerator
+
+let singleNumber x =
+  let str = x.ToString()
+  let list = LazyList.ofSeq str |> tokenize
+  match list.TryHead with
+    | Some (Ok ({ Token = Number x})) -> x = str
+    | _ -> false
+
+let cfg =
+  { FsCheckConfig.defaultConfig with
+      arbitrary = [typeof<ScientificGenerators>]
+  }
+
+[<Tests>]
+let properties =
+  testList "Number properties" [
+    testProperty "floats can be tokenized" <| fun (x: NormalFloat) -> singleNumber x
+    testProperty "ints can be tokenized" <| fun (x: int) -> singleNumber x
+    testPropertyWithConfig
+      cfg
+      "scientific notation can be tokenized"
+      (fun (x: Scientific) -> singleNumber x.Num)
+  ]
+
+[<Tests>]
+let tests =
+  testList "Number tests" [
+    testCase "Tokenizes really huge numbers" <| (fun _ ->
+      let digits = List.replicate 500 '9'
+      let subject = Array.ofList digits |> System.String
+      Expect.isTrue (singleNumber subject) "Failed to tokenize huge number"
+    )
+  ]
---
+++ JsonStream.Tests/ScienceGenerator.fs
@@ -0,0 +1,22 @@
+module ScienceGenerator
+
+open FsCheck
+
+let sign = Gen.elements [ "+"; "-"; ""; ]
+
+let e = Gen.elements [ "e"; "E"; ]
+
+let num = Arb.generate<uint32>
+
+let float = Arb.generate<NormalFloat>
+
+type Scientific = { Num: string }
+
+let scientific = Gen.map4 (fun num e sign pow ->
+  let numStr = num.ToString()
+  let powStr = pow.ToString()
+  { Num = System.String.Concat [ numStr; e; sign; powStr; ] }) float e sign num
+
+type ScientificGenerators =
+  static member ScientificNotation() =
+    Arb.fromGen scientific
